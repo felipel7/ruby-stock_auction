eae8a0c2c55ec6986bf4b9d76c9a760495f560c2
felipel7 <felipelseven@gmail.com>
2023-05-01 11:23:05 -0300

    chore: generate devise views

--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_04_30_233140) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "name"
     t.string "cpf"
     t.integer "role"
     t.datetime "created_at", null: false
