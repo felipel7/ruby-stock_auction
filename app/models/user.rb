ce24b6405359e9b16bd45a91d0bf4749cf2ad330
felipel7 <felipelseven@gmail.com>
2023-05-01 11:27:11 -0300

    feat(user): add validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,23 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  enum role: { user: 0, admin: 5 }
+
+  after_initialize :set_default_role, :if => :new_record?
+  before_save :check_email_domain
+
+  validates :cpf, presence: true, uniqueness: true, length: { is: 11 }
+
+  private
+
+  def set_default_role
+    self.role ||= :user
+  end
+
+  def check_email_domain
+    if self.email.end_with?("@leilaodogalpao.com.br")
+      self.role = :admin
+    end
+  end
 end
