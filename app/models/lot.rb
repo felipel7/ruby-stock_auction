f00c00e0587a80b4a3a0fc1cfb182b6e44d39278
felipel7 <felipelseven@gmail.com>
2023-05-06 08:38:55 -0300

    feat: add association between user and lot

--- app/models/lot.rb
+++ app/models/lot.rb
@@ -1,19 +1,18 @@
 class Lot < ApplicationRecord
-  # TODO: belongs to admin lots
-  # TODO: has many bids
-  # TODO: has many lots products
+  belongs_to :user, foreign_key: :register_by_id
+  belongs_to :approved_by, class_name: "User", foreign_key: "approved_by_id", optional: true
 
-  enum status: { pending: 0, approved: 5, sold: 10 }, _default: :pending
+  enum status: { pending: 0, approved: 5, sold: 10, inactive: 15 }, _default: :pending
 
   before_validation :generate_code, on: :create
+  validates :batch_code, uniqueness: true
   validates :batch_code, :start_date, :end_date, :min_value, :min_allowed_difference, presence: true
   validates :min_value, :min_allowed_difference, numericality: { greater_than: 0 }
-
   validates :start_date, comparison: { greater_than: Time.now,
                                        message: "deve ser posterior à hora atual" }
-
   validates :end_date, comparison: { greater_than: :start_date,
                                      message: "deve ser posterior à data de início" }
+  validate :validate_different_users
 
   private
 
@@ -21,5 +20,11 @@ class Lot < ApplicationRecord
     letters = [*("A".."Z")].sample(3).join
     numbers = SecureRandom.random_number(999_999).to_s.rjust(6, "0")
     self.batch_code = letters + numbers
+  end
+
+  def validate_different_users
+    if self.register_by_id == self.approved_by_id
+      self.errors.add(:aprovado_por_id, "deve ser diferente do usuário que cadastrou o lote")
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,22 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  enum role: { user: 0, admin: 5 }
+  has_many :lots
 
-  before_validation :valid_cpf
+  enum role: { user: 0, admin: 5 }
   after_initialize :set_default_role, :if => :new_record?
+  before_validation :valid_cpf
   before_save :check_email_domain
 
   validates :cpf, presence: true, uniqueness: true, length: { is: 11 }
 
   def is_admin?
     self.role == "admin"
+  end
+
+  def name
+    self.email.split("@")[0].capitalize
   end
 
   private
@@ -37,29 +40,25 @@ class User < ApplicationRecord
     cpf_code = self.cpf[9..-1]
     total = 0
     factor = 10
-    primo = 11
+    prime = 11
 
-    # calc first digit
     cpf_numbers.each_char do |n|
       total += n.to_i * factor
       factor -= 1
     end
-    rest_second_to_last = total % primo
-    rest_second_to_last = primo - rest_second_to_last
-    first_check_digit = rest_second_to_last >= 10 ? 0 : rest_second_to_last
+    rest_second_to_last_number = prime - (total % prime)
+    first_digit = rest_second_to_last_number >= 10 ? 0 : rest_second_to_last_number
 
-    # calc second digit
     factor = 11
     total = 0
-    (cpf_numbers + first_check_digit.to_s).each_char do |n|
+    (cpf_numbers + first_digit.to_s).each_char do |n|
       total += n.to_i * factor
       factor -= 1
     end
-    rest_to_last = total % primo
-    rest_to_last = primo - rest_to_last
-    second_check_digit = rest_to_last >= 10 ? 0 : rest_to_last
+    rest_last_number = prime - (total % prime)
+    second_digit = rest_last_number >= 10 ? 0 : rest_last_number
 
-    result = first_check_digit.to_s + second_check_digit.to_s != cpf_code
+    result = first_digit.to_s + second_digit.to_s != cpf_code
 
     if result
       self.errors.add(:cpf, "inválido")
