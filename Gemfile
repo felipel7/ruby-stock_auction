8e134faa7c5a178123e0222593fce770ec35a556
felipel7 <felipelseven@gmail.com>
2023-05-14 08:55:09 -0300

    chore: add active storage for uploading images

--- Gemfile
+++ Gemfile
@@ -22,6 +22,7 @@ gem "tzinfo-data", platforms: %i[ mingw mswin x64_mingw jruby ]
 gem "devise"
 gem "sass-rails"
 gem "font-awesome-rails"
+gem "image_processing", ">= 1.2"
 
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
--- config/application.rb
+++ config/application.rb
@@ -5,7 +5,7 @@ require "rails"
 require "active_model/railtie"
 # require "active_job/railtie"
 require "active_record/railtie"
-# require "active_storage/engine"
+require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
 # require "action_mailbox/engine"
@@ -33,5 +33,7 @@ module AuctionApp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+
+    config.action_view.field_error_proc = Proc.new { |html_tag, instance| html_tag.html_safe }
   end
 end
