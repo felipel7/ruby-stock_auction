6c1c20ec90fe3a691b142d798491b9d678cfb107
felipel7 <felipelseven@gmail.com>
2023-05-26 15:50:07 -0300

    test(bid): increment bid specs

---
+++ spec/system/bid/user_access_bids_index_spec.rb
@@ -0,0 +1,103 @@
+require "rails_helper"
+
+describe "A página de lances é acessa pelo" do
+  context "usuário" do
+    it "e retorna todos os seus lances" do
+      first_admin = User.create!(email: "maria@leilaodogalpao.com.br", cpf: "03507869098", password: "123123")
+      second_admin = User.create!(email: "felipe@leilaodogalpao.com.br", cpf: "14367226085", password: "123123")
+      user = User.create!(email: "felipe@gmail.com", cpf: "70587229004", password: "123123")
+      category = Category.create!(name: "Eletrônicos")
+      product = Product.create!(
+        name: "Monitor LG",
+        description: "Monitor de 24 polegadas da marca LG...",
+        weight: 1500,
+        width: 100,
+        height: 80,
+        depth: 15,
+        category: category,
+      )
+      lot = Lot.create!(
+        batch_code: "EOR661430",
+        start_date: 1.second.from_now,
+        end_date: 1.day.from_now,
+        min_value: 2500,
+        min_allowed_difference: 35,
+        register_by_id: first_admin.id,
+        approved_by_id: second_admin.id,
+      )
+      lot.products << product
+      lot.approved!
+
+      travel_to 1.hour.from_now do
+        login_as(user)
+        visit root_path
+        within "aside" do
+          click_on "Lotes"
+        end
+        click_on "Dar Lance"
+        fill_in "bid-input", with: 50
+        click_on "Dar Lance"
+        within "aside" do
+          click_on "Lances"
+        end
+
+        within(:css, 'section:has(h2:contains("Meus Lances Vencedores"))') do
+          expect(page).to have_content "Lote: EOR661430"
+          expect(page).to have_content "R$ 2.500,00"
+        end
+
+        within(:css, 'section:has(h2:contains("Meu Histórico de Lances"))') do
+          expect(page).to have_content "Lote: EOR661430"
+          expect(page).to have_content "R$ 2.500,00"
+        end
+      end
+    end
+  end
+
+  context "admin" do
+    it "e retorna o lance de todos os usuários" do
+      first_admin = User.create!(email: "maria@leilaodogalpao.com.br", cpf: "03507869098", password: "123123")
+      second_admin = User.create!(email: "felipe@leilaodogalpao.com.br", cpf: "14367226085", password: "123123")
+      first_user = User.create!(email: "felipe@gmail.com", cpf: "70587229004", password: "123123")
+      second_user = User.create!(email: "maria@gmail.com", cpf: "48092119082", password: "123123")
+      category = Category.create!(name: "Eletrônicos")
+      product = Product.create!(
+        name: "Monitor LG",
+        description: "Monitor de 24 polegadas da marca LG...",
+        weight: 1500,
+        width: 100,
+        height: 80,
+        depth: 15,
+        category: category,
+      )
+      lot = Lot.create!(
+        batch_code: "EOR661430",
+        start_date: 1.second.from_now,
+        end_date: 1.day.from_now,
+        min_value: 2500,
+        min_allowed_difference: 35,
+        register_by_id: first_admin.id,
+        approved_by_id: second_admin.id,
+      )
+      lot.products << product
+      lot.approved!
+
+      travel_to 1.hour.from_now do
+        Bid.create!(user: first_user, lot: lot, amount: 45)
+        Bid.create!(user: second_user, lot: lot, amount: 50)
+
+        login_as(first_admin)
+        visit root_path
+        within "aside" do
+          click_on "Lances"
+        end
+
+        expect(page).to have_content "EOR661430"
+        expect(page).to have_content "felipe@gmail.com"
+        expect(page).to have_content "45"
+        expect(page).to have_content "maria@gmail.com"
+        expect(page).to have_content "50"
+      end
+    end
+  end
+end
--- spec/system/bid/user_register_new_bid_spec.rb
+++ spec/system/bid/user_register_new_bid_spec.rb
@@ -24,22 +24,22 @@ describe "Usuário da um novo lance" do
       register_by_id: first_admin.id,
       approved_by_id: second_admin.id,
     )
-    lot.update(status: :approved)
     lot.products << product
-    lot.update(start_date: 1.minute.ago)
-    lot.reload
+    lot.approved!
 
-    login_as(user)
-    visit root_path
-    within "aside" do
-      click_on "Lotes"
+    travel_to 1.hour.from_now do
+      login_as(user)
+      visit root_path
+      within "aside" do
+        click_on "Lotes"
+      end
+      click_on "Dar Lance"
+      fill_in "bid-input", with: 30
+      click_on "Dar Lance"
+
+      expect(page).to have_content "Não foi possível dar o lance"
+      expect(page).to have_content "Valor deve ser maior que o mínimo permitido para cada lance"
     end
-    click_on "Dar Lance"
-    fill_in "bid-input", with: 30
-    click_on "Dar Lance"
-
-    expect(page).to have_content "Não foi possível dar o lance"
-    expect(page).to have_content "Valor deve ser maior que o mínimo permitido para cada lance"
   end
 
   it "e falha quando o lote ainda não está em andamento" do
@@ -66,7 +66,7 @@ describe "Usuário da um novo lance" do
       approved_by_id: second_admin.id,
     )
     lot.products << product
-    lot.update(status: :approved)
+    lot.approved!
 
     login_as(user)
     visit root_path
@@ -105,20 +105,22 @@ describe "Usuário da um novo lance" do
       approved_by_id: second_admin.id,
     )
     lot.products << product
-    lot.update(status: :approved)
+    lot.approved!
     lot.update(start_date: 1.minute.ago)
 
-    login_as(user)
-    visit root_path
-    within "aside" do
-      click_on "Lotes"
-    end
-    click_on "Dar Lance"
-    fill_in "bid-input", with: 50
-    click_on "Dar Lance"
+    travel_to 1.hour.from_now do
+      login_as(user)
+      visit root_path
+      within "aside" do
+        click_on "Lotes"
+      end
+      click_on "Dar Lance"
+      fill_in "bid-input", with: 50
+      click_on "Dar Lance"
 
-    expect(page).to have_content "O lance foi atualizado com sucesso."
-    expect(page).to have_content "Lance atual: R$ 2.550,00"
+      expect(page).to have_content "O lance foi atualizado com sucesso."
+      expect(page).to have_content "Lance atual: R$ 2.550,00"
+    end
   end
 
   it "e falha quando o usuário já possui o maior lance" do
@@ -145,24 +147,66 @@ describe "Usuário da um novo lance" do
       approved_by_id: second_admin.id,
     )
     lot.products << product
-    lot.update(status: :approved)
-    lot.update(start_date: 1.minute.ago)
+    lot.approved!
 
-    login_as(user)
-    visit root_path
-    within "aside" do
-      click_on "Lotes"
-    end
-    click_on "Dar Lance"
-    fill_in "bid-input", with: 50
-    click_on "Dar Lance"
-    within "aside" do
-      click_on "Lotes"
-    end
-    click_on "Dar Lance"
-    fill_in "bid-input", with: 50
-    click_on "Dar Lance"
+    travel_to 1.hour.from_now do
+      login_as(user)
+      visit root_path
+      within "aside" do
+        click_on "Lotes"
+      end
+      click_on "Dar Lance"
+      fill_in "bid-input", with: 50
+      click_on "Dar Lance"
+      within "aside" do
+        click_on "Lotes"
+      end
+      click_on "Dar Lance"
+      fill_in "bid-input", with: 50
+      click_on "Dar Lance"
 
-    expect(page).to have_content "Não foi possível dar o lance. O usuário já possui o último lance."
+      expect(page).to have_content "Não foi possível dar o lance. O usuário já possui o último lance."
+    end
+  end
+end
+
+describe "Admin tenta dar um lance" do
+  it "e não consegue" do
+    first_admin = User.create!(email: "maria@leilaodogalpao.com.br", cpf: "03507869098", password: "123123")
+    second_admin = User.create!(email: "felipe@leilaodogalpao.com.br", cpf: "14367226085", password: "123123")
+    category = Category.create!(name: "Eletrônicos")
+    product = Product.create!(
+      name: "Monitor LG",
+      description: "Monitor de 24 polegadas da marca LG...",
+      weight: 1500,
+      width: 100,
+      height: 80,
+      depth: 15,
+      category: category,
+    )
+    lot = Lot.create!(
+      batch_code: "EOR661430",
+      start_date: 1.second.from_now,
+      end_date: 1.day.from_now,
+      min_value: 2500,
+      min_allowed_difference: 35,
+      register_by_id: first_admin.id,
+      approved_by_id: second_admin.id,
+    )
+    lot.products << product
+    lot.approved!
+
+    travel_to 1.hour.from_now do
+      login_as(first_admin)
+      visit root_path
+      within "aside" do
+        click_on "Lotes"
+      end
+      click_on "Dar Lance"
+      fill_in "bid-input", with: 50
+      click_on "Dar Lance"
+
+      expect(page).to have_content "Administradores não podem dar lances."
+    end
   end
 end
