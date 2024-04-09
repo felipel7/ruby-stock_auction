c2e7a746408f2bd96bfeb3b714ff88c57b2e73bc
felipel7 <felipelseven@gmail.com>
2024-04-09 11:46:52 -0300

    refactor: update seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_07_11_134116) do
+ActiveRecord::Schema[7.0].define(version: 2024_03_31_183928) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -112,6 +112,8 @@ ActiveRecord::Schema[7.0].define(version: 2023_07_11_134116) do
     t.integer "role"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["cpf"], name: "index_users_on_cpf"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,19 +2,19 @@
 first_category = Category.create!(name: 'Eletrônicos')
 Category.create!(name: 'Cozinha')
 Category.create!(name: 'Vestuário')
-Category.create!(name: 'Livros')
-Category.create!(name: 'Esportes')
+fourth_category = Category.create!(name: 'Livros')
+fifth_category = Category.create!(name: 'Esportes')
 
 # Produtos
 first_product = Product.create!(
-  name: 'Monitor LG',
-  description: 'Monitor de 32 polegadas da marca LG...',
-  weight: 1500,
-  width: 100,
-  height: 80,
-  depth: 15,
+  name: 'PlayStation 5',
+  description: 'PlayStation 5 console da Sony...',
+  weight: 4500,
+  width: 40,
+  height: 30,
+  depth: 10,
   category: first_category,
-  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/monitor-lg.webp').to_s, 'image/webp')
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/ps5.png').to_s, 'image/png')
 )
 second_product = Product.create!(
   name: 'Smartphone Samsung',
@@ -24,27 +24,27 @@ second_product = Product.create!(
   height: 15,
   depth: 8,
   category: first_category,
-  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/smart-sam.webp').to_s, 'image/webp')
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/smartphones.png').to_s, 'image/png')
 )
 third_product = Product.create!(
-  name: 'Notebook Dell',
-  description: 'Notebook Dell Inspiron 15 com processador Intel Core i5...',
-  weight: 2000,
-  width: 36,
-  height: 2,
-  depth: 24,
-  category: first_category,
-  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/note-dell.webp').to_s, 'image/webp')
+  name: 'Coleção Senhor dos Anéis',
+  description: 'Trilogia épica de J.R.R. Tolkien...',
+  weight: 3000,
+  width: 20,
+  height: 25,
+  depth: 10,
+  category: fourth_category,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/book-lor.png').to_s, 'image/png')
 )
 fourth_product = Product.create!(
-  name: 'Smartwatch Apple',
-  description: 'Smartwatch Apple Watch Series 6 com caixa de 44mm...',
-  weight: 47,
-  width: 3,
-  height: 4,
-  depth: 1,
-  category: first_category,
-  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/smart-watch.webp').to_s, 'image/webp')
+  name: 'Livro Python',
+  description: 'Livro sobre a linguagem python...',
+  weight: 600,
+  width: 15,
+  height: 20,
+  depth: 2,
+  category: fourth_category,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/book-python.png').to_s, 'image/png')
 )
 fifth_product = Product.create!(
   name: 'Câmera Sony',
@@ -56,15 +56,39 @@ fifth_product = Product.create!(
   category: first_category,
   photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/sony-cam.webp').to_s, 'image/webp')
 )
+sixth_product = Product.create!(
+  name: 'Xbox Series X',
+  description: 'Xbox Series X console da Microsoft...',
+  weight: 5000,
+  width: 30,
+  height: 15,
+  depth: 10,
+  category: first_category,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/xbox.png').to_s, 'image/png')
+)
+seventh_product = Product.create!(
+  name: 'Bicicleta',
+  description: 'Mountain bike...',
+  weight: 12000,
+  width: 70,
+  height: 110,
+  depth: 20,
+  category: fifth_category,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/bike.webp').to_s, 'image/webp')
+)
 
 # Usuários regulares
 User.create!(
+  first_name: 'felipe',
+  last_name: 'silva',
   email: 'felipe@gmail.com',
   cpf: '33665186005',
   password: '123123'
 )
 
 User.create!(
+  first_name: 'maria',
+  last_name: 'silva',
   email: 'maria@gmail.com',
   cpf: '39889813033',
   password: '123123'
@@ -72,11 +96,15 @@ User.create!(
 
 # Admins
 first_admin = User.create!(
+  first_name: 'maria',
+  last_name: 'silva',
   email: 'maria@leilaodogalpao.com.br',
   cpf: '03507869098',
   password: '123123'
 )
 second_admin = User.create!(
+  first_name: 'fernando',
+  last_name: 'silva',
   email: 'fernando@leilaodogalpao.com.br',
   cpf: '64063418057',
   password: '123123'
@@ -90,7 +118,8 @@ first_lot = Lot.create!(
   min_value: 2500,
   min_allowed_difference: 35,
   register_by_id: first_admin.id,
-  approved_by_id: second_admin.id
+  approved_by_id: second_admin.id,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/smartphones-2.png').to_s, 'image/png')
 )
 first_lot.products << second_product
 first_lot.approved!
@@ -102,7 +131,8 @@ second_lot = Lot.create!(
   min_value: 1500,
   min_allowed_difference: 25,
   register_by_id: first_admin.id,
-  approved_by_id: second_admin.id
+  approved_by_id: second_admin.id,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/cameras.png').to_s, 'image/png')
 )
 second_lot.products << fifth_product
 second_lot.approved!
@@ -123,9 +153,23 @@ third_lot = Lot.create!(
   min_value: 1200,
   min_allowed_difference: 20,
   register_by_id: second_admin.id,
-  approved_by_id: first_admin.id
+  approved_by_id: first_admin.id,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/books.png').to_s, 'image/png')
 )
-third_lot.products << first_product
 third_lot.products << third_product
 third_lot.products << fourth_product
 third_lot.approved!
+
+fourth_lot = Lot.create!(
+  batch_code: 'FAE172457',
+  start_date: 2.hours.from_now,
+  end_date: 12.hours.from_now,
+  min_value: 2900,
+  min_allowed_difference: 40,
+  register_by_id: second_admin.id,
+  approved_by_id: first_admin.id,
+  photo: Rack::Test::UploadedFile.new(Rails.root.join('app/assets/images/products/xbox.png').to_s, 'image/png')
+)
+fourth_lot.products << first_product
+fourth_lot.products << sixth_product
+fourth_lot.approved!
